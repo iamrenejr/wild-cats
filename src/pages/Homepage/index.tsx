49d54a6e49860a046d7d6c58514fb0ac689ecce2
Rene Calunsag <rene.calunsag@nixplay.com>
2022-03-24 22:14:05 +0800

    Initial file reorganization and dependencies added. No significant code changes.

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../assets/images/logo.svg';
+import './Homepage.scss';
 
-function App() {
+const Homepage = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Homepage;
